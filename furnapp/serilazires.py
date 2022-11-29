632e5c3420a8ece32f0e3386df9414938d5c1af1
durbek11 <104496416+durbek11@users.noreply.github.com>
2022-11-29 14:34:09 +0000

    add API

--- furnapp/serilazires.py
+++ furnapp/serilazires.py
@@ -1,3 +1,9 @@
 from dataclasses import field
 from .models import *
 from rest_framework import serilaizers
+
+
+class firstAPI(serializers.ModelSerializer):
+    class Meta:
+        model = firstAPI
+        fields = '__all__'
