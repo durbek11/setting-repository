3757330b25499e8b2c226ae513bb13d6599cd892
durbek11 <104496416+durbek11@users.noreply.github.com>
2022-12-02 10:32:33 +0000

    addAPI

--- furnapp/serilazires.py
+++ furnapp/serilazires.py
@@ -7,3 +7,8 @@ class firstAPI(serializers.ModelSerializer):
     class Meta:
         model = MyUser
         fields = '__all__'
+
+class ProfileAPI(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
